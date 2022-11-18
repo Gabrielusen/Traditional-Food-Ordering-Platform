9a8ccaf7fca2037acc5e54ebbbf2f8617251ab54
gabriel ufot <gabrielufot23@gmail.com>
2022-11-18 16:20:31 +0100

    templates redesign

--- food/urls.py
+++ food/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import index
+from .views import food_list, food_details
+
+
+app_name = 'food'
 
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', food_list, name='food_list'),
+    path('<slug:food_category_slug>/', food_list, name='food_list_by_food_category'),
+    path('<int:id>/<slug:slug>/', food_details, name='food_details')
 ]
--- food/views.py
+++ food/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import FoodCategory, Food
 
 
-def index(request):
-    """
-    :return: food view for page templates
-    """
-    return HttpResponse('Hello World')
+def food_list(request, food_category_slug=None):
+    food_category = None
+    food_categories = FoodCategory.objects.all()
+    foods = Food.objects.filter(available=True)
+    if food_category_slug:
+        food_category = get_object_or_404(FoodCategory, slug=food_category_slug)
+        foods = foods.filter(food_category=food_category)
+    return render(request,
+                  'index.html',
+                  {'food_category': food_category,
+                   'food_categories': food_categories,
+                   'foods': foods})
+
+
+def food_details(request, id, slug):
+    food = get_object_or_404(Food,
+                             id=id,
+                             slug=slug,
+                             available=True)
+    return render(request, 'detail.html', {'food': food})
